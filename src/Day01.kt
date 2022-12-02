c89ab68dd859c6f68a3555ad606ca8eac3640552
Hotkeyyy <64286456+Hotkeyyy@users.noreply.github.com>
2022-12-02 20:06:29 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,39 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String) {
+        val elfs = input.split("\r\n\r\n")
+        println(findHighestElf(elfs))
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String) {
+        val elfs = input.split("\r\n\r\n").toMutableList()
+        var r = 0
+        for (i in 0..2) {
+            val elf = findHighestElf(elfs)
+            elfs.removeAt(elf.second)
+            r += elf.first
+        }
+        println(r)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val input = File("src/Day01.txt").readText()
+    part2(input)
+}
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+fun findHighestElf(elfs: List<String>): Pair<Int, Int> {
+    var result = 0
+    var index = 0
+    elfs.forEachIndexed { ind, it ->
+        val cals = it.split("\r\n")
+        var a = 0
+        cals.forEach { s ->
+            a += s.toInt()
+        }
+        if (a > result) {
+            result = a
+            index = ind
+        }
+    }
+    return Pair(result, index)
 }
